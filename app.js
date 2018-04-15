56b15b785f5e282cceb3fd6d09753bebed24d718
gergana <gergana_ruse@yahoo.com>
2018-04-15 10:52:47 +0100

    updated

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const createActivity = require('./controllers/createActivity');
+const getActivities = require('./controllers/getActivities');
 
 const app = express();
 
 app.post('/profile/activities', createActivity);
+app.get('/profile/activities', getActivities);
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
 
